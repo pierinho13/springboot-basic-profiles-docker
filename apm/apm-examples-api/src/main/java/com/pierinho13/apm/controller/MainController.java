f1161653400bb1b4c51f74741a818d141c4dc58b
Piero Rospigliosi <pierinho13@gmail.com>
2026-01-28 12:54:42 +0100

    fix error

--- apm/apm-examples-api/src/main/java/com/pierinho13/apm/controller/MainController.java
+++ apm/apm-examples-api/src/main/java/com/pierinho13/apm/controller/MainController.java
@@ -2,20 +2,18 @@ package com.pierinho13.apm.controller;
 
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
-	
 
     @GetMapping("/")
-    public Map<String, Object> echo(@RequestParam(defaultValue = "OK") String value) {
+    public Map<String, Object> index(@RequestParam(defaultValue = "OK") String value) {
         return Map.of(
                 "value", value,
                 "ts", System.currentTimeMillis()
         );
     }
-
 }
